2a6e9d2bb07d567d8f278acc28e5d50477367fe7
wildchamo <wildchamo@gmail.com>
2023-04-01 14:39:23 -0500

    utilización de NavLink para darle color al componente en el caso que esté activo

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Menu() {
   return (
     <nav>
-      <ul>
+      {/* <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
           <Link to="/blog">Blog</Link>
         </li>
-      </ul>
+      </ul> */}
+      <li>
+        <NavLink
+          style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/blog">Blog</NavLink>
+      </li>
     </nav>
   );
 }
 
+//navlink es muy bellako
 export default Menu;
