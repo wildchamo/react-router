d5c9c7d9a488712a8968529f08526964dce0aa95
wildchamo <wildchamo@gmail.com>
2023-04-03 10:12:21 -0500

    useNavigate es muy poderoso

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import {blogdata} from "./blogdata.js"
 
 
 function BlogPost() {
   const {slug} = useParams();
+  const navigation= useNavigate();
 
   const blogpost =blogdata.find(post => post.slug===slug)
+
+  const returnToBlog = ()=>{
+    navigation('/blog')
+  }
+
   return (
     <>
       <h2>{blogpost.title}</h2>
+      <button onClick={returnToBlog}>Volver al blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
     </>
