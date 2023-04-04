fc242715ed7a6641ac1e83b6897a25bed6a0ff4f
wildchamo <wildchamo@gmail.com>
2023-04-03 20:40:56 -0500

    manejo de rutas públicas y privadas

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 function Menu() {
   const routes = [
@@ -7,40 +8,57 @@ function Menu() {
       id: 1,
       to: "/",
       text: "Home",
+      private: false,
     },
     {
       id: 2,
       to: "/blog",
       text: "Blog",
+      private: false,
     },
     {
       id: 3,
       to: "/profile",
       text: "Profile",
+      private: true,
     },
     {
       id: 4,
       to: "/login",
       text: "login",
+      publicOnly: true,
+      private: false,
     },
     {
       id: 5,
       to: "/logout",
       text: "logout",
+      private: true,
     },
   ];
+
+  const auth = useAuth();
+
   return (
     <nav>
-      {routes.map((route) => (
-        <li key={route.to}>
-          <NavLink
-            to={route.to}
-            style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
-          >
-            {route.text}
-          </NavLink>
-        </li>
-      ))}
+      {routes.map((route) => {
+        if (route.publicOnly && auth.user) {
+          return null;
+        }
+        if (route.private && !auth.user) {
+          return null;
+        }
+        return (
+          <li key={route.to}>
+            <NavLink
+              to={route.to}
+              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        );
+      })}
     </nav>
   );
 }
